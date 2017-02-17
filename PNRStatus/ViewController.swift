367ebfcdd93b34fa8539a9ff354ad63ce77491d6
Mohanesh <meetmohanesh@gmail.com>
2017-02-18 00:24:21 +0530

    Response handled using Sessions

--- PNRStatus/ViewController.swift
+++ PNRStatus/ViewController.swift
@@ -9,17 +9,52 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var PNRTextField: UITextField!
+    @IBOutlet weak var responseTextView: UITextView!
+    
+    var passengerList = [Any](){
+        didSet{
+            updatePNRList()
+        }
+    }
+    
+    @IBAction func SubmitPNR(_ sender: UIButton) {
+        let textVal = PNRTextField.text!
+        let apiKey: String = "prhvu3wk"
+        let urlString: String = "http://api.railwayapi.com/pnr_status/pnr/\(textVal)/apikey/\(apiKey)/"
+        let postURL = URL(string: urlString)
+        print(apiKey, urlString)
+        let urlRequest = URLRequest(url: postURL!)
+        let session = URLSession.shared
+        let task = session.dataTask(with: urlRequest, completionHandler: {(data, response, error) in
+            guard let data = data, error == nil else{ return }
+            
+            do{
+                let json = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as! [String:Any]
+                let temp = json["passengers"]
+                self.passengerList = [temp!];
+                print(temp!)
+            }catch let error as NSError{
+                print(error)
+            }
+            
+        })x`
+        task.resume()
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    func updatePNRList(){
+       // print(passengerList)
+    }
+    
 }
 
